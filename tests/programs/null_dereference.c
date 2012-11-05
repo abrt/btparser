3f90b21407542c79b7086410f466386be4e02c6e
Martin Milata <mmilata@redhat.com>
2012-11-05 12:57:29 +0100

    Add simple unwind test

--- tests/programs/null_dereference.c
+++ tests/programs/null_dereference.c
@@ -1,6 +1,25 @@
-int main(int argc, char** argv)
+// gcc -fno-inline -o null_dereference null_dereference.c
+void a(void)
 {
     int *p = 0;
     *p = 10;
+}
+
+void b(int i)
+{
+    if (i == 0)
+        a();
+    else
+        b(i-1);
+}
+
+void c(void)
+{
+    b(1);
+}
+
+int main(int argc, char** argv)
+{
+    c();
     return 0;
 }
